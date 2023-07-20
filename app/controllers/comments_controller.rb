b8e55e0c8d3320fca8838389b86fad33a1853f69
kny19 <ksx-ny.19@au.com>
2023-07-20 12:14:04 +0900

    commentsコントローラーにshowと他追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,40 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    Comment.create(comment_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    comment.update(comment_params)
+    redirect_to root_path
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user, :image, :text)
+  end
+
 end
