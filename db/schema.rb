fc58e2ba7f85dee0112c2ab2ab5f8072433e1fb9
shiotanishi <139023486+shiotanishi@users.noreply.github.com>
2023-07-20 18:07:23 +0900

    ログインの有無で処理を変える

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_024424) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_031000) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "user", default: "", null: false
     t.string "email", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_024424) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
