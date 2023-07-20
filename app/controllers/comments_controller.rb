be2f4c19b47ad014e7ffbb2c038d39c46d9df6f9
kny19 <ksx-ny.19@au.com>
2023-07-20 14:53:32 +0900

    updateアクション追加！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,17 @@ class CommentsController < ApplicationController
   def edit
     @comment = comment.find(params[:id])
   end
+
+  def update
+    comment = Comment.find(params[:id])
+    comment.update(comment_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end
+
